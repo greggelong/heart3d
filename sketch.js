67f3d29978f9cdb938dbcced540d89fe57592647
Greg K <gkreisman_cep@qq.com>
2025-07-31 17:50:53 +0200

    first commit

---
+++ sketch.js
@@ -0,0 +1,49 @@
+let model3D;
+let font;
+
+function preload() {
+  model3D = loadModel("assets/model.obj", true);
+  font = loadFont("assets/NotoSansSC-VariableFont_wght.ttf"); // your .ttf file here
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  textFont(font);
+  textSize(32);
+  textAlign(CENTER, CENTER);
+}
+
+function draw() {
+  background(0);
+
+  // Spinning 3D model
+  push();
+  scale(3);
+  rotateY(millis() / 1000);
+  rotateX(millis() / 1000);
+  rotateZ(millis() / 1000);
+  ambientLight(255);
+  directionalLight(255, 255, 255, 0.5, 1, 0);
+  ambientMaterial(255, 0, 0);
+  noStroke();
+  model(model3D);
+  pop();
+
+  // Scrolling 3D Text
+  push();
+  fill(255);
+  textFont(font);
+  textSize(400);
+  textStyle(BOLD);
+
+  let txt = "艺术工人  520 Art Workers 520";
+  let textW = textWidth(txt);
+
+  // Total scroll distance: from (width + textW) ➝ -textW
+  let totalScroll = width + textW * 2;
+  let scrollX = width + textW - ((frameCount * 2) % totalScroll);
+
+  translate(scrollX, -height / 4, 0);
+  text(txt, 0, 0);
+  pop();
+}
